b618edb2e24449ca6705a9e24376e48c40128d8e
Josep Sànchez Arbona <47741862+daskza19@users.noreply.github.com>
2022-09-07 20:39:05 +0200

    Base Structured Web with Index
    Index is now without active setion feedback and have some position between header and first section. TO FIX!

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-var rellax = new Rellax ('.rellax')
+var rellax = new Rellax ('.rellax');
+
+var lastId,
+topMenu = $("#sticky-projects-index"),
+topMenuHeight = topMenu.outerHeight()+1,
+menuItems = topMenu.find("a"),
+scrollItems =menuItems.map(function(){
+    var item = $($(this).attr("href"));
+    if(item.length){return item;}
+})

e60ba9906c339cb2e45378e252032bc3a57f14e1
Josep Sànchez Arbona <47741862+daskza19@users.noreply.github.com>
2022-09-10 12:47:56 +0200

    First Project Scroller BASE Done
    Now the automatization to pass the actual project adds the extra time from manual changing project in the following project, not the actual showing manual.

--- index.js
+++ index.js
@@ -1,10 +1,68 @@
 var rellax = new Rellax ('.rellax');
 
-var lastId,
-topMenu = $("#sticky-projects-index"),
-topMenuHeight = topMenu.outerHeight()+1,
-menuItems = topMenu.find("a"),
-scrollItems =menuItems.map(function(){
-    var item = $($(this).attr("href"));
-    if(item.length){return item;}
-})
+let count=1;
+var timer = 2000;
+document.getElementById("radio1").checked=true;
+
+mainInterval();
+function mainInterval(){
+    SetCounter();
+    nextImage();
+    setTimeout(mainInterval, timer);
+}
+
+function nextImage(){
+    count++;
+    if(count>5){
+        count=1;
+    }
+    document.getElementById("radio"+count).checked=true;
+}
+
+function SetCounter(){
+    if(document.getElementById("radio1").checked==true){
+        if(count!=1){
+            count=1;
+            timer=15000;
+        }
+        else{
+            timer = 7000;
+        }
+    }
+    if(document.getElementById("radio2").checked==true){
+        if(count!=2){
+            count=2;
+            timer=15000;
+        }
+        else{
+            timer = 7000;
+        }
+    }
+    if(document.getElementById("radio3").checked==true){
+        if(count!=3){
+            count=3;
+            timer=15000;
+        }
+        else{
+            timer = 7000;
+        }
+    }
+    if(document.getElementById("radio4").checked==true){
+        if(count!=4){
+            count=4;
+            timer=15000;
+        }
+        else{
+            timer = 7000;
+        }
+    }
+    if(document.getElementById("radio5").checked==true){
+        if(count!=5){
+            count=5;
+            timer=15000;
+        }
+        else{
+            timer = 7000;
+        }
+    }
+}

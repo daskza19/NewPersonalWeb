af5d4d64ec94b1f243ffac11941d1b8f07d7c810
Josep Sànchez Arbona <47741862+daskza19@users.noreply.github.com>
2022-09-10 21:09:02 +0200

    First Two Projects and some improvements in nav colors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var rellax = new Rellax ('.rellax');
 var count=1;
 
 function rightProject(){
+    SetCounter();
     count++;
     if(count>5){
         count=1;
@@ -11,9 +12,28 @@ function rightProject(){
 }
 
 function leftProject(){
+    SetCounter();
     count--;
     if(count<=0){
         count=5;
     }
     document.getElementById("radio" + count).checked = true;
+}
+
+function SetCounter(){
+    if(document.getElementById("radio1").checked==true){
+        count=1;
+    }
+    if(document.getElementById("radio2").checked==true){
+        count=2;
+    }
+    if(document.getElementById("radio3").checked==true){
+        count=3;
+    }
+    if(document.getElementById("radio4").checked==true){
+        count=4;
+    }
+    if(document.getElementById("radio5").checked==true){
+        count=5;
+    }
 }

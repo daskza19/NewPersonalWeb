c0f241504c79ce8a54bd8cab45c00e59739bbaab
Josep Sànchez Arbona <47741862+daskza19@users.noreply.github.com>
2022-09-10 17:22:56 +0200

    Manual Scroll DONE

--- index.js
+++ index.js
@@ -1,68 +1,19 @@
 var rellax = new Rellax ('.rellax');
 
-let count=1;
-var timer = 2000;
-document.getElementById("radio1").checked=true;
+var count=1;
 
-mainInterval();
-function mainInterval(){
-    SetCounter();
-    nextImage();
-    setTimeout(mainInterval, timer);
-}
-
-function nextImage(){
+function rightProject(){
     count++;
     if(count>5){
         count=1;
     }
-    document.getElementById("radio"+count).checked=true;
+    document.getElementById("radio" + count).checked = true;
 }
 
-function SetCounter(){
-    if(document.getElementById("radio1").checked==true){
-        if(count!=1){
-            count=1;
-            timer=15000;
-        }
-        else{
-            timer = 7000;
-        }
+function leftProject(){
+    count--;
+    if(count<=0){
+        count=5;
     }
-    if(document.getElementById("radio2").checked==true){
-        if(count!=2){
-            count=2;
-            timer=15000;
-        }
-        else{
-            timer = 7000;
-        }
-    }
-    if(document.getElementById("radio3").checked==true){
-        if(count!=3){
-            count=3;
-            timer=15000;
-        }
-        else{
-            timer = 7000;
-        }
-    }
-    if(document.getElementById("radio4").checked==true){
-        if(count!=4){
-            count=4;
-            timer=15000;
-        }
-        else{
-            timer = 7000;
-        }
-    }
-    if(document.getElementById("radio5").checked==true){
-        if(count!=5){
-            count=5;
-            timer=15000;
-        }
-        else{
-            timer = 7000;
-        }
-    }
+    document.getElementById("radio" + count).checked = true;
 }
